d823f9a6163d738af10eaab67371d1a141c0c882
andriislipenko <andrii.slipenko@gmail.com>
2021-11-09 14:52:05 +0200

    task 2 using regexp

--- homework_2/task_2.rb
+++ homework_2/task_2.rb
@@ -1,19 +1,15 @@
-def numeric?(input)
-  !Float(input).nil? rescue false
-end
-
 loop do
   puts "Enter an arbitrary number:"
-  input = gets.chomp
+  input = gets.strip
 
-  unless numeric?(input)
+  unless /^-?\d+$/.match?(input)
     puts "Sorry, only numbers are accepted!"
     next
   end
 
-  break if input.count("1") > 1
+  break if /(1\d*){2,}/.match?(input)
 
-  puts "Result: #{Float(input).to_i * 256}"
+  puts "Result: #{input.to_i * 256}"
 end
 
 puts "You broke the rule"
