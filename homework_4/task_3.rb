fa2fbc1502b2cf4626827b80f7bb1ff0451f2892
andriislipenko <andrii.slipenko@gmail.com>
2021-12-04 12:32:14 +0200

    task 3 done

---
+++ homework_4/task_3.rb
@@ -0,0 +1,33 @@
+module Accessible
+  def attr_accessor(*fields)
+    fields.each do |field|
+      self.define_method(field) do
+        value = self.instance_variable_get("@#{field.to_s}")
+        puts 'this field is empty' if value.nil?
+        value
+      end
+
+      self.define_method("#{field.to_s}=".to_sym) do |value|
+        self.instance_variable_set("@#{field.to_s}", value)
+      end
+    end
+  end
+end
+
+class Test
+  extend Accessible
+
+  attr_accessor :aaa, :bbb, :ccc, :ddd
+
+  def initialize
+    @aaa = 5
+    @bbb = 'd'
+    @ccc = 3
+    @ddd = 3
+  end
+end
+
+test = Test.new
+p test.aaa
+test.aaa = nil
+p test.aaa
