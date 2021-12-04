cbd84f1ccdfeee6986b44661cab4a4523543b167
andriislipenko <andrii.slipenko@gmail.com>
2021-12-04 11:55:21 +0200

    refactored task 2

--- homework_4/task_2.rb
+++ homework_4/task_2.rb
@@ -6,25 +6,30 @@ module Sortable
       first, second = second, first unless asc
 
       if first.respond_to?(field) && second.respond_to?(field)
-        first_value = first.send(field)
-        second_value = second.send(field)
-
-        next -1 if first_value.nil? || first_value == false
-        next 1 if second_value.nil? || second_value == false
-        next first_value <=> second_value
+        next compare_values(first.send(field), second.send(field))
       end
 
-      [first, second].each do |element|
-        unless element.respond_to?(field)
-          puts "#{element.inspect} does not respond to #{field}"
-          failed << element
-        end
-      end
-
+      [first, second].each { |element| failed << element unless test_response(element, field) }
       next 0
     end
 
-    puts failed.to_s
+    puts "Failed: #{failed.to_s}"
     arr.difference failed
+  end
+
+  private
+
+  def compare_values(first, second)
+    return -1 if first.nil? || first == false
+    return 1 if second.nil? || second == false
+    first <=> second
+  end
+
+  def test_response(element, field)
+    unless element.respond_to?(field)
+      puts "#{element.inspect} does not respond to #{field}"
+      return false
+    end
+    true
   end
 end
